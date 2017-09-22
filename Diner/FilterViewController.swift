403b36cd57f2c7dc1d064557d666f6367b588ab5
Prithvi Prabahar <prithvi923@gmail.com>
2017-09-21 22:54:36 -0700

    Add modal filter page

---
+++ Diner/FilterViewController.swift
@@ -0,0 +1,41 @@
+//
+//  FilterViewController.swift
+//  Diner
+//
+//  Created by Prithvi Prabahar on 9/21/17.
+//  Copyright © 2017 Prithvi Prabahar. All rights reserved.
+//
+
+import UIKit
+
+class FilterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func cancelPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func searchPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
